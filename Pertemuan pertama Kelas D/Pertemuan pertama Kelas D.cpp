a4e11b9b3469eb921fc77b0d4cdfeb4f539a8433
gibranrafii <gibranrafi21@gmail.com>
2024-02-27 11:18:07 +0700

    Add project files.

---
+++ Pertemuan pertama Kelas D/Pertemuan pertama Kelas D.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{//begin
+	//Numeric nAlas, nTinggi, nLuas
+	//display 'Masukkan Alasnya= '
+	//accept nAlas
+	//display 'Masukkan Tingginya= '
+	//accept nTinggi
+	//compute nLuas = 1/2 * nAlas * nTinggi
+	//display 'Luasnya adalah = ' +nLuas
+
+	int nAlas, nTinggi;
+	double nLuas;
+	cout << "Masukkan Alasnya= ";
+	cin >> nAlas;
+	cout << "Masukkan Tingginya= " ;
+	cin >> nTinggi;
+	nLuas = 0.5 * nAlas * nTinggi;
+	cout << "Luasnya Adalah= " << nLuas << endl;
+
+	system("pause");
+}//end
